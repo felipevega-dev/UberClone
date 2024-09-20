9a8acc531e49795034f80d780845220ac3619f83
felipevega-dev <felipevega.dev@gmail.com>
2024-09-19 23:35:01 -0300

    API MAPS

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,4 +1,3 @@
-import { useUser } from "@clerk/clerk-expo";
 import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RideCard from "@/components/RideCard";
@@ -111,8 +110,6 @@ const recentRides = [
 ];
 
 export default function Page() {
-  const { user } = useUser();
-
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
