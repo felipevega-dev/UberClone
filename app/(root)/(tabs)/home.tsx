ee12ba541a2fc3121ea0eb4e3a92543d111a7ce6
felipevega-dev <felipevega.dev@gmail.com>
2024-09-24 13:14:21 -0300

    Barra de busqueda, Buscar viaje component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -15,6 +15,7 @@ import GoogleTextInput from "@/components/GoogleTextInput";
 import { icons, images } from "@/constants";
 import { useLocationStore } from "@/store";
 import { useEffect, useState } from "react";
+import { router } from "expo-router";
 
 const recentRides = [
   {
@@ -131,7 +132,14 @@ export default function Page() {
   const [hasPermissions, setHasPermissions] = useState(false);
 
   const handleSignOut = () => {};
-  const handleDestinationPress = () => {};
+  const handleDestinationPress = (location: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    setDestinationLocation(location);
+    router.push("/(root)/find-ride");
+  };
 
   useEffect(() => {
     const requestLocation = async () => {
---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const ConfirmRide = () => {
+  <View>
+    <Text>Confirmar Viaje</Text>
+  </View>;
+};
+
+export default ConfirmRide;
